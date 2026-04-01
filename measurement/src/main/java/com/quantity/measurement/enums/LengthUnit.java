27dd05c000ba932b038bb65c12c6e5abf5a7497b
babita7056 <babita7056.ca22@chitkara.edu.in>
2026-04-01 15:35:04 +0530

    UC4 — Extended Unit Support

--- measurement/src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ measurement/src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -1,16 +1,31 @@
 package com.quantity.measurement.enums;
 
 public enum LengthUnit {
+
     FEET(1.0),
-    INCH(1.0 / 12.0);
+
+    INCH(1.0/12),
+
+    YARD(3.0),
+
+    CM(0.0328084);
 
     private final double toFeetFactor;
 
+
+
     LengthUnit(double toFeetFactor) {
+
         this.toFeetFactor = toFeetFactor;
+
     }
 
+
+
     public double toFeet(double value) {
+
         return value * toFeetFactor;
+
     }
+
 }
